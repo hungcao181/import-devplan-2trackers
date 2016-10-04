67941785506d429468a8dda95bfcdc31d13bf373
hung cao <hungcao181@gmail.com>
2016-10-04 16:10:24 +0700

    read config of Pivotal from setting inputs, load saved input from file

---
+++ importer.js
@@ -0,0 +1,88 @@
+'use strict';
+var fs = require('fs');
+var filepath = './config/data.secret';
+var client;
+
+var async = require('async');
+var jQuery = require('jquery-deferred');
+
+function importer (options) {
+    
+    var answer = 1;
+    //we can input argument when run npm start and check process.argv[2] instead. Below is example of using readline
+    if (answer == 1 || answer == 0 || answer == null) {
+        
+        client = require('./services/pivotaltracker-service');
+        client.config(options)
+        loadDataByStructure();
+        
+    };
+    if (answer == 2)
+    {
+        client = require('./services/gitlab-service');
+        loadDataByStructure();
+    }
+    if (answer == 99) {
+        console.log('listing your gitlab project');
+        client = require('./services/gitlab-service');
+        client.getProjects();
+    }
+}
+
+function loadDataByStructure() {
+    fs.readFile(filepath, {'encoding': 'utf8'}, function (err, data) {
+        if (err) throw err;
+        // var formatedData = data.replace(/sprint:|epic:/gi, function myFunction(x){return x.toUpperCase();});
+        var sprints = data.split(/SPRINT[ ]*:?[ ]*/i).filter(function(s) {return s != ''}).reverse();
+        async.eachSeries(
+            sprints,
+            processSprint,
+            function(err) {
+                if (err) console.log('error ',err);    
+            }
+        );
+    });
+}
+
+function processSprint(sprint, done) {
+    var epics = sprint.split(/EPIC[ ]*:?[ ]*/i).filter(function(e) { return e != ''; });
+    var sprintName = epics.shift();//.replace(/(?:\r\n|\r|\n)/g, '');
+    console.log(sprintName);
+    jQuery.when(createMilestone(sprintName)).done(
+        function(milestoneID) {
+            async.eachSeries(
+                epics.reverse(),
+                processEpic.bind(null, milestoneID), //thanks this post http://stackoverflow.com/questions/20882892/pass-extra-argument-to-async-map
+                function(err) {
+                    if (err) console.log('error ',err);
+                    done();
+                }
+            );
+        }
+    );
+}
+
+function processEpic(milestone, epic, done) {
+    var stories = epic.split(/\r?\n/).map(function(s) {return s.trim()}).filter(function(story){ return story != '' });
+    var label = stories.shift();//first item is epic name
+    // console.log('stories ', stories);
+    async.eachSeries(
+        stories.reverse().map(function(s) {return {'title':s, 'labels': [label]}}),
+        createStory.bind(null, milestone),
+        function(err) {
+            if (err) console.log('error ',err);
+            done();
+        }
+    );
+}
+
+function createMilestone(milestone) {
+    return client.createMilestone(milestone);
+}
+
+function createStory(milestone, story, done) {
+    // done();
+    client.createStory(milestone, story,done);
+}
+
+module.exports = importer;
---
+++ renderer.js
@@ -0,0 +1,60 @@
+// This file is required by the index.html file and will
+// be executed in the renderer process for that window.
+// All of the Node.js APIs are available in this process.
+  'use strict';
+  const ipc = require('electron').ipcRenderer;
+  let loadButton = document.getElementById("load-button");
+  let dataArea = document.getElementById("data");
+  let workingFile = document.getElementById("working-file");
+  let saveButton = document.getElementById("save-button");
+  var fs = require('fs');
+
+  loadButton.addEventListener('click', function (e) {
+    ipc.send('open-file-dialog');
+  });
+
+  
+  ipc.on('selected-file', function (e, path) {
+    if (path) {
+      let fileName = path[0];
+      workingFile.innerHTML= fileName;
+      fs.readFile(fileName, 'utf8', (err, data) => {
+        if (data) dataArea.value = data;
+      })
+    }
+  })
+
+  saveButton.addEventListener('click', (e) => {
+    let fileName = workingFile.innerHTML;
+    //if working on a file already, then save to that file. If not, let user get a file to save to
+    if (fileName) {
+      let data = dataArea.value;
+      fs.writeFile(fileName, data, 'utf8', (err) => {
+        if (err) throw err;
+        console.log(`It\' saved to ${fileName}`);
+      })
+    } else {
+      ipc.send('save-dialog');
+    }
+  });
+
+  ipc.on('saved-file', (e, path) => {
+    
+    if (path) {
+      workingFile.innerHTML = path;
+      let data = dataArea.value;
+      fs.writeFile(path, data, 'utf8', (err) => {
+        if (err) throw err;
+        console.log(`It\' saved to ${path}`);
+      })
+    }
+  })
+
+  ipc.on('config-available', (e, savedConfig) => {
+    if (savedConfig) {
+      console.log("I get it, config is available");
+      document.getElementById("prjid").value = savedConfig.pivotaltracker.project;
+      document.getElementById("pttoken").value = savedConfig.pivotaltracker.apitoken;
+    }
+  })
+
