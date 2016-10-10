ac69ce6c33d94e3613223f4bf07c2882467106ad
hung cao <hungcao181@gmail.com>
2016-10-10 17:52:03 +0700

    move importer code into folder so it is easy to copy

--- main.js
+++ main.js
@@ -13,10 +13,6 @@ let mainWindow
 const ipc = require('electron').ipcMain;
 const dialog = require('electron').dialog;
 
-var fs = require('fs');
-
-let importer = require('./importer.js');
-
 function createWindow () {
   // Create the browser window.
   mainWindow = new BrowserWindow({width: 800, height: 600})
@@ -61,60 +57,4 @@ app.on('activate', function () {
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
-ipc.on('open-file-dialog', function (e) {
-  dialog.showOpenDialog({
-    properties: ['openFile', '']
-  }, function (files) {
-    if (files) e.sender.send('selected-file', files);
-  })
-})
-
-ipc.on('save-dialog', (e) => {
-  const options = {
-    title: 'Save data to file',
-    filters: [{name: 'text file', extensions: ['txt']}]
-  };
-  dialog.showSaveDialog(options, (fileName) => {
-    e.sender.send('saved-file', fileName);
-  })
-})
-
-ipc.on('document-ready', (e) => {
-  // console.log("document ready, from main");
-  fs.readFile('./config/client.secret', {'encoding': 'utf8'}, function (err, data) {
-      if (err) return;
-      savedConfig = JSON.parse(data);
-      e.sender.send('config-available', savedConfig)
-  });
-})
-
-
-//following functions are not used, just demo for it's capacity to open another window inside app
-function createExternalUrlWindow (anUrl) {
-  // Create the browser window.
-  let aWindow = new BrowserWindow({width: 800, height: 600})
-
-  // and load the index.html of the app.
-  aWindow.loadURL(anUrl)
-
-
-  // Emitted when the window is closed.
-  aWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    aWindow = null
-  })
-}
-//this is triggered from a renderer by issueing ipc.send(openExternalUrl, anUrl)
-ipc.on('openExternalUrl', (e, anUrl) => {
-  createExternalUrlWindow(anUrl);
-})
-
-ipc.on('doimport', (e, options) => {
-  console.log('yes, sir, import now');
-  importer(options);
-})
-ipc.on('importsprintdone', (e) => {
-  e.sender.send('updateimportstatus');
-})
+require('./importer/main.js').run(ipc);
